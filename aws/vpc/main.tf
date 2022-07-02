153c2ee42b03c8c5064812d27041bb761202cf7f
nopecho <ch.jooon@gmail.com>
2022-07-02 16:44:34 +0900

    terraform aws vpc module 분리

--- aws/vpc/main.tf
+++ aws/vpc/main.tf
@@ -1,7 +1,26 @@
-resource "aws_vpc" "vpc" {
-  cidr_block = "10.0.0.0/22"
-
+resource "aws_vpc" "my-vpc" {
+  cidr_block = var.vpc_cidr_block
   tags = {
-    name = "my-vpc"
+    "domain" = "my-domain"
+    "terraform" = "support"
+    "creator" = "ch.jooon"
+  }
+}
+
+resource "aws_subnet" "public" {
+  cidr_block = var.public_subnet_cidr_block
+  tags = {
+    "domain" = "my-domain"
+    "terraform" = "support"
+    "creator" = "ch.jooon"
+  }
+}
+
+resource "aws_subnet" "private" {
+  cidr_block = var.private_subnet_cidr_block
+  tags = {
+    "domain" = "my-domain"
+    "terraform" = "support"
+    "creator" = "ch.jooon"
   }
 }
---
+++ aws/vpc/variables.tf
@@ -0,0 +1,19 @@
+variable "vpc_cidr_block" {
+  type = string
+}
+
+variable "public_subnet_cidr_block" {
+  type = string
+}
+
+variable "private_subnet_cidr_block" {
+  type = string
+}
+
+variable "AWS_ACCESS_KEY_ID" {
+  type = string
+}
+
+variable "AWS_SECET_ACCESS_KEY" {
+  type = string
+}
